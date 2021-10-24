78562b0571bf7532dc6c5f0a1a4a52df5d042105
Valdir <valdir.silva@fcamara.com.br>
2021-10-24 04:54:34 -0300

    Customização tela notícias

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "login"
+  }
+  ,
+  {
+    path: "login", component: LoginComponent
+  },
+  {
+    path: "noticias", component: NoticiasComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+  
+})
+export class AppRoutingModule { }
